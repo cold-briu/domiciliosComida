186632cdcd02d544628412570c1bc629822fa5d2
Andres F <andresavena19@gmail.com>
2019-09-07 00:34:50 -0500

    pre deploy

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('uelcom to comida rapida'))
+router.get('/oe', (req, res) => res.send('uelcom to comida rapida'))
 
 
 
