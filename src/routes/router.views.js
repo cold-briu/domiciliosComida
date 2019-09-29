6aa5b4912344939dea2d062c99032441c3cce249
oela <oela@azulBook.local>
2019-09-28 21:49:08 -0500

    splash 95%, no sé donde están los estilos globales

--- src/routes/router.views.js
+++ src/routes/router.views.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/login', (req, res) => {
-    res.render('partials/login')
-})
+router.get('/', (req, res) => res.status(200).render('pages/splash'))
+
+router.get('/login', (req, res) => res.status(200).render('partials/login'))
 
 module.exports = router;
