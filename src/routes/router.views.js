21bf3864aa7d8cc702fc38b476e1c5774e1deeee
cold-briu <andresavena19@gmail.com>
2019-10-04 17:54:33 -0500

    hice una vaina

--- src/routes/router.views.js
+++ src/routes/router.views.js
@@ -1,26 +1,55 @@
 const express = require('express');
 const router = express.Router();
-const fetch = require('node-fetch')
+// const fetch = require('node-fetch')
 
-const getData = async () => {
-
-    const response = await fetch('http://desarrollo.me:3000/api/menu/read', { mode: 'cors' })
-    let data = await response.json();
-
-    console.log(data);
-    return data
-}
+const Menu = require('../models/MenuModel')
 
 
+// const getData = async () => {
 
-router.get('/', (req, res) => res.status(200).render('../views/splash'))
+//     const response = await fetch('http://desarrollo.me:3000/api/menu/read', { mode: 'cors' })
+//     let data = await response.json();
 
-router.get('/login', (req, res) => res.status(200).render('../views/login'))
+//     console.log(data);
+//     return data
+// }
 
-router.get('/home', (req, res) => res.render('menu', {
-    layout: 'logged',
-    data: getData()
-}));
+
+
+router.get('/', (req, res) => res.status(200).render('../views/splash', {
+    title: 'Splash'
+}))
+
+router.get('/login', (req, res) => res.status(200).render('../views/login', { title: 'Login' }))
+
+router.get('/register', (req, res) => res.status(200).render('../views/register', { title: 'Registrate' }))
+
+
+router.get('/home', (req, res, next) => Menu.find({}).then(
+    (data) => res.render('menu', {
+        title: 'Perritos · Menu',
+        layout: 'logged',
+        data: data
+    })
+));
+
+
+
+router.get('/pedido', (req, res, next) => Menu.find({}).then(
+    (data) => res.render('pedido', {
+        title: 'Perritos · Pedido',
+        layout: 'logged',
+        data: data
+    })
+));
+
+
+router.get('/entrega', (req, res, next) => Menu.find({}).then(
+    (data) => res.render('entrega', {
+        title: 'Perritos · Entrega',
+        layout: 'logged'
+    })
+));
 
 
 module.exports = router;
